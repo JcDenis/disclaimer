38a69aa830b1b722947ba4b44824ce7685debd35
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-09 18:23:59 +0200

    Use Core\Url

--- src/UrlHandler.php
+++ src/UrlHandler.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Dotclear\Plugin\disclaimer;
 
 use Dotclear\App;
-use Dotclear\Core\Frontend\Url;
+use Dotclear\Core\Url;
 use Dotclear\Helper\Network\Http;
 use Dotclear\Helper\Network\UrlHandler as HelperHandler;
 
